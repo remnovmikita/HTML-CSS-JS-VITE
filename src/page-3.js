54dd2d751f0b6cdc17680922a79a619a5ae5288f
Nikita <remnovmikita@gmail.com>
2026-01-29 17:44:32 +0200

    fix form

--- src/page-3.js
+++ src/page-3.js
@@ -1,29 +1,44 @@
-import { message } from "./js/iziToast"
+// import { message } from "./js/iziToast"
 
- const btn = document.querySelectorAll('.form')
-const formBox = document.querySelector(".form-box")
-const log = document.querySelector('.btn-log');
-const reg = document.querySelector('.btn-reg')
+//  const btn = document.querySelectorAll('.form')
+// const formBox = document.querySelector(".form-box")
+// const log = document.querySelector('.btn-log');
+// const reg = document.querySelector('.btn-reg')
 
 
-log.addEventListener('click', () =>{
-    formBox.classList.add('active');
-    console.log("Ви кликнули на логин")
-})
-reg.addEventListener('click', () => {
-    formBox.classList.remove('active');
-    console.log("Ви кликнули на регистрацию")
-})
-//  btn.forEach(findForm => {
-//     findForm.addEventListener("submit", e =>{
-//         if(e.target.classList.contains("form-log")){
-//             message("success", "LOGIN")
-//             formBox.classList.add('active')
-//         }else if(e.target.classList.contains("form-registration")){
-//             message('success', "REGISTRATION")
-//             formBox.classList.remove('active')
-//         }
-//     })
-//  })
+// log.addEventListener('click', () =>{
+//     formBox.classList.add('active');
+//     console.log("Ви кликнули на логин")
+// })
+// reg.addEventListener('click', () => {
+//     formBox.classList.remove('active');
+//     console.log("Ви кликнули на регистрацию")
+// })
+// //  btn.forEach(findForm => {
+// //     findForm.addEventListener("submit", e =>{
+// //         if(e.target.classList.contains("form-log")){
+// //             message("success", "LOGIN")
+// //             formBox.classList.add('active')
+// //         }else if(e.target.classList.contains("form-registration")){
+// //             message('success', "REGISTRATION")
+// //             formBox.classList.remove('active')
+// //         }
+// //     })
+// //  })
  
- console.log(formBox)
+//  console.log(formBox)
+
+const body = document.body;
+const btnLog = document.querySelector(".btn-log");
+const formBox = document.querySelector('.form-box')
+const btnReg = document.querySelector('.btn-reg');
+
+btnReg.addEventListener("click", () =>{
+    formBox.classList.add('active');
+    body.classList.add('active')
+})
+
+btnLog.addEventListener("click", () =>{
+    formBox.classList.remove('active');
+    body.classList.remove('active')
+})
