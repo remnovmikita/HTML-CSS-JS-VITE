6274f0daaa4e4ff2e18302aca87418c14ccb2494
Nikita <remnovmikita@gmail.com>
2026-01-31 14:20:59 +0200

    fix page 2 page 3

--- src/js/createNewAdd.js
+++ src/js/createNewAdd.js
@@ -1,12 +1,14 @@
 export function createNew(arr) {
   return arr
-    .map(({id, name, email, nikname}) => `
-      <div>
-        <p>${id}</p>
-        <p>${name}</p>
-        <p>${email}</p>
-        <p>${nikname}</p>
+    .map(({id, name, email, nikname, inLogin}) => `
+      <div class="users-profile">
+        <p>"id користувача" <span class="users-data">${id}</span></p>
+        <p>"Ім'я користувача" <span class="users-data">${name}</span></p>
+        <p>"Пошта користувача" <span class="users-data">${email}</span></p>
+        <p>"Нік користувача" <span class="users-data">${nikname}</span></p>
+        <p>Залогінився -> <span class="users-data">${inLogin}</span></p>
         <button class="closeBtn" data-id="${id}">Видалити</button>
+
       </div>
     `)
     .join("");
--- src/page-3.js
+++ src/page-3.js
@@ -28,8 +28,18 @@ btn.forEach(findForm => {
                 const input = form.querySelectorAll("input");
                 const formValue = {};
 
+                    let password = "";
+                    let password2 = "";
+                    let userEmail ="";
 
                 input.forEach(x =>{
+                    if(x.name === "password"){
+                        password = x.value
+                    }else if(x.name === "password2"){
+                        password2 = x.value;
+                    }else{
+                        formValue[x.name] = x.value;
+                    }
                     if(x.name !== "password2" && x.name !== "password"){
                         formValue[x.name] = x.value
                     }
@@ -37,21 +47,42 @@ btn.forEach(findForm => {
                 })
         if(form.classList.contains("form-login")){
             
-            message("success", "LOGIN")
-            console.log(formValue);
+            input.forEach(x => {
+                if(x.name === "email"){
+                    userEmail = x.value;
+                }else if(x.name === "password"){
+                    password = x.value;
+                }
+            })
+            const user = arr.find(y => y.email === userEmail && y.password === password);
+           if(user){
+            message("success", "Login")
+            user.history = user.history || [];
+            user.history.push({action: "login", date: new Date().toISOString})
+            user.inLogin = "Залогінився"
+            localStorage.setItem('save', JSON.stringify(arr));
+            form.reset();
+           }else{
+            message("error", "Невірний логін або пароль")
+            return
+           }
+            }else if(form.classList.contains("form-register")){
+           if (password !== password2){
+            message("error", "Поролі не схожі")
+            return
+           }
             
-            
-            
-        }else if(form.classList.contains("form-register")){
-           
-            message('success', "REGISTRATION")
-            console.log(formValue)
 
             counter += 1;
+            formValue.inLogin = "Ще не увійшов"
             formValue.id = counter;
+            formValue.password = password;
             arr.push(formValue)
             localStorage.setItem('save', JSON.stringify(arr));
             form.reset();
+
+            message('success', "REGISTRATION")
+            console.log(formValue)
         }
     })
  })
